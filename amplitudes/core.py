efe579d45c53dc2f9732ec996bfe6257f2e1f459
kevinkamaras <kevin.kamaras@gmail.com>
2025-02-15 13:15:12 +0100

    created massless and massive spinor classes

--- amplitudes/core.py
+++ amplitudes/core.py
@@ -10,12 +10,12 @@ def tthgg(t1, tbar2, h3, g4, g5, hcase):
     p15 = t1 + g5
     hat4, hat5 = hp.onShell(shift, p15)
     hatp15 = t1 + hat5
-    d1 = hp.ttg(t1, hatp15, hat5, hcase) * tthg(-hatp15, tbar2, h3, hat4, hcase) / (p15**2 - hp.mtop**2)
+    d1 = hp.ttg(t1, hatp15, hat5, hcase) * tthg(-hatp15, tbar2, h3, hat4, hcase) / (abs(p15)**2 - hp.mtop**2)
 
     p24 = tbar2 + g4
     hat4, hat5 = hp.onShell(shift, p24)
     hatp24 = tbar2 + hat4
-    d2 = tthg(t1, -hatp24, h3, hat5, hcase) * hp.ttg(hatp24, tbar2, hat4, hcase) / (p24**2 - hp.mtop**2)
+    d2 = tthg(t1, -hatp24, h3, hat5, hcase) * hp.ttg(hatp24, tbar2, hat4, hcase) / (abs(p24)**2 - hp.mtop**2)
     
     amp = d1 + d2
     return amp
@@ -30,17 +30,17 @@ def tthggg(t1, tbar2, h3, g4, g5, g6, hcase):
     p16 = t1 + g6
     hat5, hat6 = hp.onShell(shift, p16)
     hatp16 = t1 + hat6
-    d1 = hp.ttg(t1, hatp16, hat6) * tthgg(-hatp16, tbar2, h3, g4, hat5) / (p16**2 - hp.mtop**2)
+    d1 = hp.ttg(t1, hatp16, hat6) * tthgg(-hatp16, tbar2, h3, g4, hat5) / (abs(p16)**2 - hp.mtop**2)
 
     p136 = t1 + h3 + g6
     hat5, hat6 = hp.onShell(shift, p136)
     hatp136 = t1 + h3 + hat6
-    d2 = tthg(t1, hatp136, h3, hat6) * hp.ttgg(-hatp136, tbar2, g4, hat5) / (p136**2 - hp.mtop**2)
+    d2 = tthg(t1, hatp136, h3, hat6) * hp.ttgg(-hatp136, tbar2, g4, hat5) / (abs(p136)**2 - hp.mtop**2)
 
     p45 = g4 + g5
     hat5, hat6 = hp.onShell(shift, p45)
     hatp45 = g4 + hat5
-    d3 = tthgg(t1, tbar2, h3, -hatp45, hat6) * hp.ggg(hatp45, g4, hat5) / p45**2
+    d3 = tthgg(t1, tbar2, h3, -hatp45, hat6) * hp.ggg(hatp45, g4, hat5) / abs(p45)**2
 
     amp = d1 + d2 + d3
     return amp
@@ -51,17 +51,17 @@ def tthqqg(t1, tbar2, h3, q4, qbar5, g6, hcase):
     p16 = t1 + g6
     hat5, hat6 = hp.onShell(shift, p16)
     hatp16 = t1 + hat6
-    d1 = hp.ttg(t1, hatp16, hat6) * tthqq(-hatp16, tbar2, h3, q4, hat5) / (p16**2 - hp.mtop**2)
+    d1 = hp.ttg(t1, hatp16, hat6) * tthqq(-hatp16, tbar2, h3, q4, hat5) / (abs(p16)**2 - hp.mtop**2)
 
     p136 = t1 + h3 + g6
     hat5, hat6 = hp.onShell(shift, p136)
     hatp136 = t1 + h3 + hat6
-    d2 = tthg(t1, hatp136, h3, hat6) * hp.ttqq(-hatp136, tbar2, q4, hat5) / (p136**2 - hp.mtop**2)
+    d2 = tthg(t1, hatp136, h3, hat6) * hp.ttqq(-hatp136, tbar2, q4, hat5) / (abs(p136)**2 - hp.mtop**2)
 
     p45 = q4 + qbar5
     hat5, hat6 = hp.onShell(shift, p45)
     hatp45 = q4 + hat5
-    d3 = tthgg(t1, tbar2, h3, -hatp45, hat6) * hp.qqg(hatp45, q4, hat5) / p45**2
+    d3 = tthgg(t1, tbar2, h3, -hatp45, hat6) * hp.qqg(hatp45, q4, hat5) / abs(p45)**2
 
     amp = d1 + d2 + d3
     return amp
@@ -72,22 +72,22 @@ def tthgggg(t1, tbar2, h3, g4, g5, g6, g7, hcase):
     p17 = t1 + g7
     hat6, hat7 = hp.onShell(shift, p17)
     hatp17 = t1 + hat7
-    d1 = hp.ttg(t1, hatp17, hat7) * hp.tthggg(-hatp17, tbar2, h3, g4, g5, hat6) / (p17**2 - hp.mtop**2)
+    d1 = hp.ttg(t1, hatp17, hat7) * hp.tthggg(-hatp17, tbar2, h3, g4, g5, hat6) / (abs(p17)**2 - hp.mtop**2)
 
     p137 = t1 + h3 + g7
     hat6, hat7 = hp.onShell(shift, p137)
     hatp137 = t1 + h3 + hat7
-    d2 = tthg(t1, hatp137, h3, g7) * hp.ttggg(-hatp137, tbar2, g4, g5, hat6) / (p137**2 - hp.mtop**2)
+    d2 = tthg(t1, hatp137, h3, g7) * hp.ttggg(-hatp137, tbar2, g4, g5, hat6) / (abs(p137)**2 - hp.mtop**2)
 
     p456 = g4 + g5 + g6
     hat6, hat7 = hp.onShell(shift, p456)
     hatp456 = g4 + g5 + hat6
-    d3 = tthgg(t1, tbar2, h3, -hatp456, hat7) * hp.gggg(hatp456, g4, g5, hat6) / p456**2
+    d3 = tthgg(t1, tbar2, h3, -hatp456, hat7) * hp.gggg(hatp456, g4, g5, hat6) / abs(p456)**2
 
     p56 = g5 + g6
     hat6, hat7 = hp.onShell(shift, p56)
     hatp56 = g5 + hat6
-    d4 = tthggg(t1, tbar2, h3, g4, -hatp56, hat7) * hp.ggg(hatp56, g5, g6) / p56**2
+    d4 = tthggg(t1, tbar2, h3, g4, -hatp56, hat7) * hp.ggg(hatp56, g5, g6) / abs(p56)**2
 
     amp = d1 + d2 + d3 + d4
     return amp
@@ -98,22 +98,22 @@ def tthqqgg(t1, tbar2, h3, q4, qbar5, g6, g7, hcase):
     p17 = t1 + g7
     hat6, hat7 = hp.onShell(shift, p17)
     hatp17 = t1 + hat7
-    d1 = hp.ttg(t1, hatp17, hat7) * hp.tthqqg(-hatp17, tbar2, h3, q4, qbar5, hat6) / (p17**2 - hp.mtop**2)
+    d1 = hp.ttg(t1, hatp17, hat7) * hp.tthqqg(-hatp17, tbar2, h3, q4, qbar5, hat6) / (abs(p17)**2 - hp.mtop**2)
 
     p137 = t1 + h3 + g7
     hat6, hat7 = hp.onShell(shift, p137)
     hatp137 = t1 + h3 + hat7
-    d2 = tthg(t1, hatp137, h3, g7) * hp.ttqqg(-hatp137, tbar2, q4, qbar5, hat6) / (p137**2 - hp.mtop**2)
+    d2 = tthg(t1, hatp137, h3, g7) * hp.ttqqg(-hatp137, tbar2, q4, qbar5, hat6) / (abs(p137)**2 - hp.mtop**2)
 
     p456 = q4 + qbar5 + g6
     hat6, hat7 = hp.onShell(shift, p456)
     hatp456 = q4 + qbar5 + hat6
-    d3 = tthgg(t1, tbar2, h3, -hatp456, hat7) * hp.qqgg(hatp456, q4, qbar5, hat6) / p456**2
+    d3 = tthgg(t1, tbar2, h3, -hatp456, hat7) * hp.qqgg(hatp456, q4, qbar5, hat6) / abs(p456)**2
 
     p56 = qbar5 + g6
     hat6, hat7 = hp.onShell(shift, p56)
     hatp56 = qbar5 + hat6
-    d4 = tthggg(t1, tbar2, h3, q4, -hatp56, hat7) * hp.ggg(hatp56, qbar5, g6) / p56**2
+    d4 = tthggg(t1, tbar2, h3, q4, -hatp56, hat7) * hp.ggg(hatp56, qbar5, g6) / abs(p56)**2
 
     amp = d1 + d2 + d3 + d4
     return amp
--- amplitudes/helpers.py
+++ amplitudes/helpers.py
@@ -1,6 +1,57 @@
 import numpy as np
 
 mtop = 1
+eps = np.array([[0, 1], [-1, 0]])
+
+class massless:
+    '''spinors for a given massless momentum'''
+    def __init__(self, p0, p1, p2, p3):
+        m2 = abs(p0)**2 - abs(p1)**2 - abs(p2)**2 - abs(p3)**2
+        if m2 >= 1e-10:
+            raise ValueError('momentum is not massless')
+        pp = p0 + p3
+        pm = p0 - p3
+        pt = p1 + 1j*p2
+        pb = p1 - 1j*p2
+        theta = (1 + np.sign(-p0)) / 2
+        if pp == 0:
+            raise ValueError('pp = 0')
+        else:
+            self.aket = ( 1j)**theta * np.array([[-pb / np.sqrt(pp), np.sqrt(pp)]]).T
+            self.sbra = (-1j)**theta * np.array([[-pt / np.sqrt(pp), np.sqrt(pp)]])
+            self.abra = np.matmul(eps, self.aket).T
+            self.sket = np.matmul(self.sbra, eps).T
+
+class massive:
+    '''spinors for a given massive momentum'''
+    def __init__(self, p0, p1, p2, p3):
+        E = p0
+        P = np.sign(E) * np.sqrt(abs(p1)**2 + abs(p2)**2 + abs(p3)**2)
+        pp = p0 + p3
+        pm = p0 - p3
+        pt = p1 + 1j*p2
+        pb = p1 - 1j*p2
+        if pp == 0:
+            raise ValueError('pp = 0')
+        else:
+            I1 = np.sqrt((E - P) / 2 / P) * np.array([[np.sqrt(pp),  pt / np.sqrt(pp)]]).T
+            I2 = np.sqrt((E + P) / 2 / P) * np.array([[-pb / np.sqrt(pp), np.sqrt(pp)]]).T
+            J1 = np.sqrt((E + P) / 2 / P) * np.array([[-pt / np.sqrt(pp), np.sqrt(pp)]])
+            J2 = np.sqrt((E - P) / 2 / P) * np.array([[np.sqrt(pp),  pb / np.sqrt(pp)]])
+            self.aket = np.hstack([I1, I2])
+            self.sbra = np.vstack([J1, J2])
+            self.abra = np.matmul(eps, self.aket).T  
+            self.sket = np.matmul(self.sbra, eps).T
+
+def abraket(a, b):
+    '''angle braket of two spinors. 0-, 1-, or 2-dimensional output'''
+    braket = np.matmul(a.abra, b.aket)
+    return braket
+
+def sbraket(a, b):
+    '''square braket of two spinors. 0-, 1-, or 2-dimensional output'''
+    braket = np.matmul(a.sbra, b.sket)
+    return braket
 
 def BCFW(i, j, hcase):
     shift = None
