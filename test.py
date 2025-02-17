50983b57b5174c9c55f5abaddc2bed889d8121b7
kevinkamaras <kevin.kamaras@gmail.com>
2025-02-17 13:22:53 +0100

    updated test

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import numpy as np
 import amplitudes.helpers as hp
 
-t1    = hp.massive(5, 1, 1, 0)
-tbar2 = hp.massive(5, 1, 0, 1)
-g3    = hp.massless(np.sqrt(2), 1, 0, 1)
+t1    = np.array([5, 1, 1, 0])
+tbar2 = np.array([5, 1, 0, 1])
+g3    = np.array([np.sqrt(2), 1, 0, 1])
 hcase = ['+']
-ref   = hp.massless(np.sqrt(2), 1, -1, 0)
+ref   = np.array([np.sqrt(2), 1, -1, 0])
+
+spin1 = hp.massive(*t1)
+spin3 = hp.massless(*g3)
 
 amp = hp.ttg(t1, tbar2, g3, hcase, ref)
