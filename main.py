d7ff33f6215c92feab5b04c87d08c214760ad3e1
kevinkamaras <kevin.kamaras@gmail.com>
2025-03-15 12:38:23 +0100

    added all tests to main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import amplitudes.amps as amps
 import tests
 
-# how to obtain an amplitude:
-# write the (all outgoing) four-momenta involved in the amplitude as four-component lists
-# for example for tthggg:
+# How to obtain an amplitude:
+# Write the (all outgoing) four-momenta involved in the amplitude as four-component lists.
+# For example for tthggg:
 t1    = [   5,   -3, -1,  0]
 tbar2 = [   5,    0,  1, -4]
 h3    = [   5,    0,  0,  1]
@@ -11,31 +11,67 @@ g4    = [-7.5,  7.5,  0,  0]
 g5    = [-7.5, -7.5,  0,  0]
 g6    = [   1,    0,  1,  0]
 
-# choose a helicity case by listing the helicities of the massless particles as + or - in order
+# Choose a helicity case by listing the helicities of the massless particles as + or - in order
 helicity_case = ['+', '-', '-']
 
-# all the color-ordered helicity amplitudes for (tth + n partons) are in amps.py
-# to obtain a helicity amplitude call the function for the desired amplitude and helicity case
-# returns the helicity amplitude as a 2x2 matrix
-# spin of the top quark runs along axis 0, spin of the anti-top quark runs along axis 1
+# All the color-ordered helicity amplitudes for (tth + n partons) are in amps.py.
+# To obtain a helicity amplitude call the function for the desired amplitude and helicity case.
+# Returns the helicity amplitude as a 2x2 matrix.
+# Spin of the top quark runs along axis 0, spin of the anti-top quark runs along axis 1.
 tthggg_amplitude = amps.tthggg(t1, tbar2, h3, g4, g5, g6, helicity_case)
-print(f'example amplitude tthggg =\n{tthggg_amplitude}\n')
 
-# some tests that I ran to check my amplitudes:
+print(f'\nexample amplitude tthggg =\n{tthggg_amplitude}\n')
 
+
+# Some tests that I ran to check my amplitudes:
 # -----------------------------------------------------------------
-# ttqqg test: here I compare the ttqqg (no Higgs boson) amplitude I get for helicity case [-, +, +]
-# with the explicit expression found in the Campbell and Ellis paper at the bottom of page 12
-# this tests that the BCFW shift and on-shell condition are programmed correctly
-# also tests the subamplitdes ttgg, ttqq, ttg, qqg
+# ttqqg test: Here I compare the ttqqg (no Higgs boson) amplitude I get for helicity case [-, +, +]
+#             with the explicit expression found in the Campbell and Ellis paper at the bottom of page 12.
+#             This tests that the BCFW shift and on-shell condition are programmed correctly.
+#             Also tests the subamplitdes ttgg, ttqq, ttg, qqg.
 t1    = [   5,   -3, -1,  0]
 tbar2 = [   5,    0,  1, -4]
 q3    = [   5,    3,  0,  4]
 qbar4 = [-7.5,  7.5,  0,  0]
 g5    = [-7.5, -7.5,  0,  0]
 momenta = [t1, tbar2, q3, qbar4, g5]
+
 tests.ttqqg_test(momenta)
+
 # ---------------------------------------------------------------
+# ttgg test: Here I compare the ttgg amplitude squared summed over color and spins
+#            with the expression for the cross section found at the end of Gluck1978.
+#            This test makes sure that the massive and massless spinors are programmed correctly
+#            and that the matrix multiplications correspond to the correct index contractions.
+#            I work in the center of mass frame of the two massive quarks.
+#            The degrees of freedom are the two angles for the top pair,
+#            the momentum of the top pair, and the two angles for the gluons. 
 
+phiTop = 1.27
+thetaTop = 0.78
+pTop = 200
+phiGlu = 0.97
+thetaGlu = 2.5
 
+tests.ttgg_test(phiTop, thetaTop, pTop, phiGlu, thetaGlu)
 
+# ---------------------------------------------------------------
+# tthg test: Here I compare the tthg amplitude for four different reference spinors.
+#            This test checks that the amplitudes really are independent of the reference spinor.
+#            I work in the center of mass frame of the two massive quarks.
+#            The degrees of freedom are the angles and momentum for the top pair
+#            and the angles for the Higgs and gluon.
+#            The momenta I use for the reference spinors are:
+#               ref1 = [1, 1,  0, 0]
+#               ref2 = [1, 0,  1, 0]
+#               ref3 = [1, 0,  0, 1]
+#               ref4 = [5, 3, -4, 0]
+
+phiTop = 1.27
+thetaTop = 0.78
+pTop = 200
+phiGlu = 0.97
+thetaGlu = 2.5
+hcase = '+'
+
+tests.tthg_test(phiTop, thetaTop, pTop, phiGlu, thetaGlu, hcase)
--- tests.py
+++ tests.py
@@ -4,126 +4,148 @@ import amplitudes.core as core
 import amplitudes.amps as amps
 
 def ttqqg_test(momenta):
-     t1 = hp.massive(momenta[0])
-     tbar2 = hp.massive(momenta[1])
-     q3 = hp.massless(momenta[2])
-     qbar4 = hp.massless(momenta[3])
-     g5 = hp.massless(momenta[4])
-     hcase = ['-', '+', '+']
-     ttqqg = core.ttqqg(t1, tbar2, q3, qbar4, g5, hcase)
-     
-     d1 = 1j * (((t1.sbra @ (qbar4.sketabra() + g5.sketabra() ) @ t1.momentum() @ g5.sket
-      + hp.sbraket(t1, g5) @ hp.abraket(qbar4, g5) @ hp.sbraket(g5, qbar4)) @ hp.abraket(q3, tbar2)
-      + hp.abraket(t1, q3) @ g5.sbra @ t1.sketabra() @ (qbar4.momentum() + g5.momentum()) @ tbar2.sket)
-      * (qbar4.abra @ t1.momentum() @ g5.sket) / ((g5.abra @ t1.momentum() @ g5.sket)
-                                                * hp.abraket(qbar4, g5) * hp.abraket(q3, qbar4) *
-                                                (q3.sbra @ (qbar4.sketabra() + g5.sketabra()) @ t1.momentum() @ g5.sket)))
+    t1 = hp.massive(momenta[0])
+    tbar2 = hp.massive(momenta[1])
+    q3 = hp.massless(momenta[2])
+    qbar4 = hp.massless(momenta[3])
+    g5 = hp.massless(momenta[4])
+    hcase = ['-', '+', '+']
+    ttqqg = core.ttqqg(t1, tbar2, q3, qbar4, g5, hcase)
+    
+    d1 = 1j * (((t1.sbra @ (qbar4.sketabra() + g5.sketabra() ) @ t1.momentum() @ g5.sket
+    + hp.sbraket(t1, g5) @ hp.abraket(qbar4, g5) @ hp.sbraket(g5, qbar4)) @ hp.abraket(q3, tbar2)
+    + hp.abraket(t1, q3) @ g5.sbra @ t1.sketabra() @ (qbar4.momentum() + g5.momentum()) @ tbar2.sket)
+    * (qbar4.abra @ t1.momentum() @ g5.sket) / ((g5.abra @ t1.momentum() @ g5.sket)
+                                            * hp.abraket(qbar4, g5) * hp.abraket(q3, qbar4) *
+                                            (q3.sbra @ (qbar4.sketabra() + g5.sketabra()) @ t1.momentum() @ g5.sket)))
 
-     p345 = q3.vector + qbar4.vector + g5.vector
-     s345 = hp.minkowski(p345)
-     d2 = 1j * ((t1.mass * hp.sbraket(q3, g5) * hp.sbraket(qbar4, g5)**2 * hp.abraket(t1, tbar2)) 
-               / (s345 * hp.sbraket(q3, qbar4) * (q3.sbra @ (qbar4.sketabra() + g5.sketabra()) @ t1.momentum() @ g5.sket)))
-     
-     diff = ttqqg - (d1 + d2)
-     if diff.all() <= 1:
-          print('                ------------------------------')
-          print('test for ttqqg passed!\n')
-          print(f'amplitude from my ttqqg function:\n{ttqqg}\n')
-          print(f'amplitude from explicit expression in Campbell2023:\n{d1 + d2}\n')
-          print(f'diff =\n{diff}')
-          print('                ------------------------------')
-     return
+    p345 = q3.vector + qbar4.vector + g5.vector
+    s345 = hp.minkowski(p345)
+    d2 = 1j * ((t1.mass * hp.sbraket(q3, g5) * hp.sbraket(qbar4, g5)**2 * hp.abraket(t1, tbar2)) 
+            / (s345 * hp.sbraket(q3, qbar4) * (q3.sbra @ (qbar4.sketabra() + g5.sketabra()) @ t1.momentum() @ g5.sket)))
+    
+    diff = ttqqg - (d1 + d2)
+    print('\nTEST FOR ttqqg:')
+    print('----------------------------------------------')
+    if diff.all() <= 1:
+        print('test for ttqqg passed!\n')
+        print(f'amplitude from my ttqqg function:\n{ttqqg}\n')
+        print(f'amplitude from explicit expression in Campbell2023:\n{d1 + d2}\n')
+        print(f'|diff| =\n{abs(diff)}')
+        print('----------------------------------------------\n')
+    else:
+        print('----------------------------------------------')
+        print('test for ttqqg failed!')
+        print('----------------------------------------------\n')
+    return
 
-m = 171
+def ttgg_test(phitop, thetatop, ptop, phiglu, thetaglu):
+    mtop = 171
+    Etop = np.sqrt(ptop**2 + mtop**2)
+    ptopx = ptop * np.sin(thetatop) * np.cos(phitop)
+    ptopy = ptop * np.sin(thetatop) * np.sin(phitop)
+    ptopz = ptop * np.cos(thetatop)
 
-phitop = 1.27
-thetatop = 0.78
-ptop = 200
-phiglu = 0.97
-thetaglu = 2.5
+    Eglu = Etop
+    pglux = Eglu * np.sin(thetaglu) * np.cos(phiglu)
+    pgluy = Eglu * np.sin(thetaglu) * np.sin(phiglu)
+    pgluz = Eglu * np.cos(thetaglu)
 
-Etop = np.sqrt(ptop**2 + m**2)
-ptopx = ptop * np.sin(thetatop) * np.cos(phitop)
-ptopy = ptop * np.sin(thetatop) * np.sin(phitop)
-ptopz = ptop * np.cos(thetatop)
+    t1    = hp.massive([Etop, ptopx, ptopy, ptopz])
+    tbar2 = hp.massive([Etop, -ptopx, -ptopy, -ptopz])
+    g3    = hp.massless([-Eglu, pglux, pgluy, pgluz])
+    g4    = hp.massless([-Eglu, -pglux, -pgluy, -pgluz])
+    hcases = [['+', '+'],
+              ['-', '-'],
+              ['+', '-'],
+              ['-', '+']]
 
-Eglu = Etop
-pglux = Eglu * np.sin(thetaglu) * np.cos(phiglu)
-pgluy = Eglu * np.sin(thetaglu) * np.sin(phiglu)
-pgluz = Eglu * np.cos(thetaglu)
+    hcases2 = [[hcase[1], hcase[0]] for hcase in hcases]
 
-t1    = hp.massive([Etop, ptopx, ptopy, ptopz])
-tbar2 = hp.massive([Etop, -ptopx, -ptopy, -ptopz])
-g3    = hp.massless([-Eglu, pglux, pgluy, pgluz])
-g4    = hp.massless([-Eglu, -pglux, -pgluy, -pgluz])
-hcases = [['+', '+'],
-          ['-', '-'],
-          ['+', '-'],
-          ['-', '+']]
+    amps1234 = [core.ttgg(t1, tbar2, g3, g4, hcase) for hcase in hcases]
+    amps1243 = [core.ttgg(t1, tbar2, g4, g3, hcase) for hcase in hcases2]
+    subleadings = [amp1234 + amp1243 for amp1234, amp1243 in zip(amps1234, amps1243)]
 
-hcases2 = [[hcase[1], hcase[0]] for hcase in hcases]
+    N = 3
+    V = N**2 - 1
+    colorSum = []
+    for A1, A2, A3 in zip(amps1234, amps1243, subleadings):
+         colorSum += [V * (N * (abs(A1**2) + abs(A2**2)) - (1 / N) * abs(A3**2))]
 
-amps1234 = [core.ttgg(t1, tbar2, g3, g4, hcase) for hcase in hcases]
-amps1243 = [core.ttgg(t1, tbar2, g4, g3, hcase) for hcase in hcases2]
-subleadings = [amp1234 + amp1243 for amp1234, amp1243 in zip(amps1234, amps1243)]
+    spinSum = sum([np.sum(i) for i in colorSum])
 
-N = 3
-V = N**2 - 1
-colorSum = []
-for A1, A2, A3 in zip(amps1234, amps1243, subleadings):
-     colorSum += [V * (N * (abs(A1**2) + abs(A2**2)) - (1 / N) * abs(A3**2))]
+    p12 = -t1.vector - tbar2.vector
+    p13 = -t1.vector - g3.vector
+    p14 = -t1.vector - g4.vector
 
-spinSum = sum([np.sum(i) for i in colorSum])
+    s = hp.minkowski(p12)
+    t = hp.minkowski(p13)
+    u = hp.minkowski(p14)
 
-#from Gluck1978
-p12 = -t1.vector - tbar2.vector
-p13 = -t1.vector - g3.vector
-p14 = -t1.vector - g4.vector
+    Mss = (4 / s**2) * (t - mtop**2) * (u - mtop**2)
+    Mtt = (-2 / (t - mtop**2)**2) * (4 * mtop**4 - (t - mtop**2) * (u - mtop**2) + 2 * mtop**2 * (t - mtop**2))
+    Muu = (-2 / (u - mtop**2)**2) * (4 * mtop**4 - (u - mtop**2) * (t - mtop**2) + 2 * mtop**2 * (u - mtop**2))
+    Mst = (4 / (s * (t - mtop**2))) * (mtop**4 - t * (s + t))
+    Msu = (4 / (s * (u - mtop**2))) * (mtop**4 - u * (s + u))
+    Mtu = ((-4 * mtop**2) / ((t - mtop**2) * (u - mtop**2))) * (4 * mtop**2 + (t - mtop**2) + (u - mtop**2))
 
-s = hp.minkowski(p12)
-t = hp.minkowski(p13)
-u = hp.minkowski(p14)
+    result = (12 * Mss + (16 / 3) * Mtt + (16 / 3) * Muu + 6 * Mst + 6 * Msu - (2 / 3) * Mtu) * 4
 
-Mss = (4 / s**2) * (t - m**2) * (u - m**2)
-Mtt = (-2 / (t - m**2)**2) * (4 * m**4 - (t - m**2) * (u - m**2) + 2 * m**2 * (t - m**2))
-Muu = (-2 / (u - m**2)**2) * (4 * m**4 - (u - m**2) * (t - m**2) + 2 * m**2 * (u - m**2))
-Mst = (4 / (s * (t - m**2))) * (m**4 - t * (s + t))
-Msu = (4 / (s * (u - m**2))) * (m**4 - u * (s + u))
-Mtu = ((-4 * m**2) / ((t - m**2) * (u - m**2))) * (4 * m**2 + (t - m**2) + (u - m**2))
+    diff = spinSum - result
+    print('\nTEST FOR ttgg:')
+    print('----------------------------------------------')
+    if diff <= 1:
+        print('test for ttgg passed!\n')
+        print(f'cross section from my ttgg function:\n{spinSum}\n')
+        print(f'cross-section from explicit expression in Gluck1978:\n{result}\n')
+        print(f'diff =\n{diff}')
+        print('----------------------------------------------\n')
+    else:
+        print('----------------------------------------------')
+        print('test for ttgg failed!')
+        print('----------------------------------------------\n')
+    return
 
-result = (12 * Mss + (16 / 3) * Mtt + (16 / 3) * Muu + 6 * Mst + 6 * Msu - (2 / 3) * Mtu) * 4
 
-ratio = spinSum / result
-# print(f'<|M|^2> = {spinSum}')
-# print(f'<|M|^2> = {result}')
-# print(f'ratio = {ratio}')
+def tthg_test(phitop, thetatop, ptop, phiglu, thetaglu, hcase):
+    mtop = 171
+    mhig = 125
+    Etop = np.sqrt(ptop**2 + mtop**2)
+    ptopx = ptop * np.sin(thetatop) * np.cos(phitop)
+    ptopy = ptop * np.sin(thetatop) * np.sin(phitop)
+    ptopz = ptop * np.cos(thetatop)
 
-mtop = 171
-mhig = 125
+    Eglu = Etop - (mhig**2 / (4 * Etop))
+    pglux = Eglu * np.sin(thetaglu) * np.cos(phiglu)
+    pgluy = Eglu * np.sin(thetaglu) * np.sin(phiglu)
+    pgluz = Eglu * np.cos(thetaglu)
 
-phitop = 1.27
-thetatop = 0.78
-ptop = 200
-phiglu = 0.97
-thetaglu = 2.5
+    Ehig = np.sqrt(Eglu**2 + mhig**2)
+    t1    = hp.massive([Etop, ptopx, ptopy, ptopz])
+    tbar2 = hp.massive([Etop, -ptopx, -ptopy, -ptopz])
+    h3    = hp.massive([-Ehig, pglux, pgluy, pgluz])
+    g4    = hp.massless([-Eglu, -pglux, -pgluy, -pgluz])
+    ref1  = hp.massless([1, 1, 0, 0])
+    ref2  = hp.massless([1, 0, 1, 0])
+    ref3  = hp.massless([1, 0, 0, 1])
+    ref4  = hp.massless([5, 3, -4, 0])
 
-Etop = np.sqrt(ptop**2 + m**2)
-ptopx = ptop * np.sin(thetatop) * np.cos(phitop)
-ptopy = ptop * np.sin(thetatop) * np.sin(phitop)
-ptopz = ptop * np.cos(thetatop)
+    tthg1 = core.tthg(t1, tbar2, h3, g4, hcase, ref1)
+    tthg2 = core.tthg(t1, tbar2, h3, g4, hcase, ref2)
+    tthg3 = core.tthg(t1, tbar2, h3, g4, hcase, ref3)
+    tthg4 = core.tthg(t1, tbar2, h3, g4, hcase, ref4)
 
-Eglu = Etop - (mhig**2 / (4 * Etop))
-pglux = Eglu * np.sin(thetaglu) * np.cos(phiglu)
-pgluy = Eglu * np.sin(thetaglu) * np.sin(phiglu)
-pgluz = Eglu * np.cos(thetaglu)
-
-Ehig = np.sqrt(Eglu**2 + mhig**2)
-t1    = hp.massive([Etop, ptopx, ptopy, ptopz])
-tbar2 = hp.massive([Etop, -ptopx, -ptopy, -ptopz])
-h3    = hp.massive([-Ehig, pglux, pgluy, pgluz])
-g4    = hp.massless([-Eglu, -pglux, -pgluy, -pgluz])
-ref   = hp.massless([1, 1, 0, 0])
-hcase = '-'
-
-tthg = core.tthg(t1, tbar2, h3, g4, hcase, ref)
-# print(f'tthg =\n{tthg}')
+    print('\nTEST FOR tthg:')
+    print('----------------------------------------------')
+    if abs(tthg1 - tthg2).all() <= 1:
+        print('test for tthg passed!\n')
+        print(f'amplitude for ref1:\n{tthg1}\n')
+        print(f'amplitude for ref2:\n{tthg2}\n')
+        print(f'amplitude for ref3:\n{tthg3}\n')
+        print(f'amplitude for ref4:\n{tthg4}\n')
+        print('----------------------------------------------\n')
+    else:
+        print('----------------------------------------------')
+        print('test for tthg failed!')
+        print('----------------------------------------------\n')
+    return
